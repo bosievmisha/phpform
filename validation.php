a40ac2f520e7c6299159090eef205c33e31749bc
Mikhail Bosiev <mikhailbosiev@MBP-Robert-2.lan>
2024-09-12 16:03:01 +0300

    Added php validation

--- validation.php
+++ validation.php
@@ -1,2 +1,29 @@
 <?php
-    $email;
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = htmlspecialchars(trim($_POST['email']));
+        $name = htmlspecialchars(trim($_POST['name']));
+        $surname = htmlspecialchars(trim($_POST['surname']));
+        $fathername = htmlspecialchars(trim($_POST['fathername']));
+        $phone_number = htmlspecialchars(trim($_POST['phone_number']));
+        $phone_number2 = htmlspecialchars(trim($_POST['phone_number2']));
+    
+        if (empty($name) || empty($email) || empty($surname) || empty($phone_number)) {
+            echo "Все поля обязательны для заполнения.";
+            exit;
+        }
+        else{
+            echo "Ты молодец";
+            exit;
+        }
+    
+        $to = $email;
+        $subject = "Новое сообщение с сайта";
+        $message = "Имя: $name\nEmail: $email";
+        $headers = "From: {$email}";
+    
+        if (mail($to, $subject, $message, $headers)) {
+            echo "Сообщение успешно отправлено!";
+        } else {
+            echo "Ошибка при отправке сообщения.";
+        }
+    }
